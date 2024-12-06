0a2d02826baffcce6fa7b690d17ad4e872a7cad1
Nicholas Howland <nicholash@exylum.tech>
2024-12-05 23:17:40 -0800

    Create functions.sh
    added a basic template for creating functions

---
+++ Reference/functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+printFunction () {
+echo "Function with no params"
+}
+
+paramFunction(){
+echo "The first paramiter was as follows"
+echo $1
+}
+# Will print the message defined
+printFunction
+# will print the string "one"
+paramFunction one
