500f51f048cc2daccfcb69f0e35f1b367b5fa04f
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2025-03-19 09:53:17 -0800

    Update if.sh
    Consolidated functions from mutliple if files

--- Reference/if.sh
+++ Reference/if.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+## Check if a string has been set
+if [ $1 ] ; then (echo "Argument passed"); 
+else (echo "No argument passed");
+fi;
+
+## Check if multiple args have been set
+if [[ $1 && $2 ]]
+then echo "Arguments passed"
+else echo "Two arguments needed for successful execution";
+fi;
+
+## Arithmatic operations
 x=$(echo 54)
 y=$(echo 54)
 if [[ $y -lt $x ]]; then
@@ -7,4 +19,11 @@ elif [[ $y -eq $x ]]; then
 echo "y and x are equal"
 elif [[ $y -gt $x ]]; then
 echo "y is greater than x"
+fi
+
+## compare strings
+#!/bin/bash
+
+if [[ $1 == "test" ]]; then
+echo "string match";
 fi
