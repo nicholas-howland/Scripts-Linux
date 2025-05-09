b4480ea851fe686188f9179ff6e757bcb0914701
Nicholas Howland <94145193+ExylumTechnical@users.noreply.github.com>
2025-05-09 16:04:54 -0700

    Create colors.sh
    created colors reference for output beautification

---
+++ Reference/colors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo -e "\033\033[0;1m This is 1 text\033[0m
+\033[0;3m This is 3 text\033[0m
+\033[0;4m This is 4 text\033[0m
+\033[0;5m This is 5 text\033[0m
+\033[0;7m This is 7 text\033[0m
+\033[0;31m This is 31 text\033[0m
+\033[0;32m This is 32 text\033[0m
+\033[0;33m This is 33 text\033[0m
+\033[0;34m This is 34 text\033[0m
+\033[0;35m This is 35 text\033[0m
+\033[0;36m This is 36 text\033[0m
+\033[0;37m This is 37 text\033[0m
+\033[0;40m This is 40 text\033[0m
+\033[0;41m This is 41 text\033[0m
+\033[0;42m This is 42 text\033[0m
+\033[0;43m This is 43 text\033[0m
+\033[0;44m This is 44 text\033[0m
+\033[0;45m This is 45 text\033[0m
+\033[0;46m This is 46 text\033[0m
+\033[0;47m This is 47 text\033[0m"
+
