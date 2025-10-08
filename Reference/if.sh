b8fe4e1c8108915b4c764bc869529e3fea6efc8c
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2025-10-08 16:59:27 -0700

    Update if.sh
    added in more logical operators and comparisons

--- Reference/if.sh
+++ Reference/if.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
-## Check if a string has been set
-if [ $1 ] ; then (echo "Argument passed"); 
-else (echo "No argument passed");
+## Check if options are set on the command line, simple if exists then do things variuble check
+if [[ $1 && $2 ]]; then 
+echo "Two arguments passed"
+elif [ $1 ]; then
+echo "One argument passed";
+else 
+echo "Two arguments needed for successful execution";
 fi;
 
-## Check if multiple args have been set
-if [[ $1 && $2 ]]
-then echo "Arguments passed"
-else echo "Two arguments needed for successful execution";
-fi;
-
-## Arithmatic operations
+## Comparison operator examples
 x=$(echo 54)
 y=$(echo 54)
-if [[ $y -lt $x ]]; then
-echo "y is less than x"
-elif [[ $y -eq $x ]]; then
-echo "y and x are equal"
-elif [[ $y -gt $x ]]; then
-echo "y is greater than x"
+if [ $y -eq $x ]; then
+echo "y is equal to x"; fi
+if [ $y -ne $x ]; then
+echo "y is not equal to x"; fi
+if [ $y -lt $x ]; then
+echo "y is less than x"; fi
+if [ $y -le $x ]; then
+echo "y is less than or equal to x"; fi
+if [ $y -gt $x ]; then
+echo "y is greater than x"; fi
+if [ $y -ge $x ]; then
+echo "y is greater than or equal to x"; fi
+
+## logical and operator
+if [ $y -gt 1 ] &&  [ $y -lt 10 ]; then
+echo "y is greater than one and less than 10"
+fi
+## logical or opeartor
+if [ $y -gt -10 ] || [ $y -lt 10 ]; then
+echo "y is greater than one and less than 10"
 fi
 
-## compare strings
-#!/bin/bash
-
-if [[ $1 == "test" ]]; then
-echo "string match";
+if ![ $y -eq 10 ]; then
+echo "y is not equal to 10"
 fi
+
+
+
+
+
+
+
+
